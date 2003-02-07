69d5108065b281ccd93b1ce9b780fe2dab84624c
Maxim Kizub <mkizub@gmail.com>
2003-02-07 23:55:21 +0000

    Changed to use ../binkiev-0.1c.jar

--- src/make.bat
+++ src/make.bat
@@ -1,1 +1,1 @@
-c:\jdk1.3\bin\java -classpath kiev-0.1c.jar -verify kiev.Main -classpath classes -verify -p k2.prj %1 %2 %3 %4 %5 %6 %7 %8 %9
+c:\jdk1.3\bin\java -classpath ..\bin\kiev-0.1c.jar -verify kiev.Main -classpath classes -verify -g -p k2.prj %1 %2 %3 %4 %5 %6 %7 %8 %9
