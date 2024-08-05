7caffe334dc9e14288c6da32d7eb98529bfd5ca5
Maxim Kizub <mkizub@gmail.com>
2024-08-05 18:55:51 +0300

    Working on handwritten parser

--- kiev-compiler/src/main/kiev/parser/cc.bat
+++ kiev-compiler/src/main/kiev/parser/cc.bat
@@ -8,7 +8,7 @@ java -classpath "..\..\..\..\..\bin\javacc-7.0.13.jar" javacc kiev05.jj
 @ren kiev050Constants.java      kiev050Constants.kj
 @rem ren kiev050TokenManager.java   kiev050TokenManager.kj
 @del /Q ParseException.java
-@rem del /Q SimpleCharStream.java
+@del /Q SimpleCharStream.java
 @del /Q Token.java
 @del /Q TokenMgrError.java
 @del /Q TokenManager.java
